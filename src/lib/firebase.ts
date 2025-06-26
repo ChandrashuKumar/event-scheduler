926135b27127815de252cb460d6584f36dd6a466
Chandrashu Kumar <kchandrashu@gmail.com>
2025-06-26 18:52:44 +0530

    configured prisma with supabase postgres

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmt93x8zoXgzyD46mWLNh-0LwL4lJko6g",
+  authDomain: "event-scheduler-8226f.firebaseapp.com",
+  projectId: "event-scheduler-8226f",
+  storageBucket: "event-scheduler-8226f.firebasestorage.app",
+  messagingSenderId: "747318799708",
+  appId: "1:747318799708:web:867d1cf555e9003d2c6599"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
