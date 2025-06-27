2d252ccb9d8bd36c0ffac0c4d9f3034cb2ade948
Chandrashu Kumar <kchandrashu@gmail.com>
2025-06-27 14:42:17 +0530

    create group implemented

---
+++ src/app/api/group/create/route.ts
@@ -0,0 +1,53 @@
+import { auth } from '@/lib/firebase-admin';
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@/generated/prisma';
+
+const prisma = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  const authHeader = req.headers.get('Authorization');
+
+  if (!authHeader?.startsWith('Bearer ')) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  try {
+    const token = authHeader.split('Bearer ')[1];
+    const decoded = await auth.verifyIdToken(token);
+    const firebaseUID = decoded.uid;
+
+    const dbUser = await prisma.user.findUnique({
+      where: { firebaseUID },
+    });
+
+    if(!dbUser) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    const body = await req.json();
+    const { name } = body;
+
+    if (!name || name.trim() === '') {
+      return NextResponse.json({ error: 'Group name is required' }, { status: 400 });
+    }
+
+    const group = await prisma.group.create({
+      data: {
+        name,
+        createdBy: firebaseUID,
+      },
+    });
+
+    await prisma.groupMember.create({
+      data: {
+        userId: dbUser.id,
+        groupId: group.id,
+      },
+    });
+
+    return NextResponse.json(group, { status: 201 });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
