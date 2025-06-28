a47f67aa5bf4f9d049cf5a5d5f07f5865ef9ccc6
Chandrashu Kumar <kchandrashu@gmail.com>
2025-06-28 20:32:57 +0530

    deployment configs added

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,6 +1,8 @@
 import * as admin from 'firebase-admin';
-import serviceAccount from '@/lib/secrets/firebase-admin-key.json'; // adjust path if needed
-
+//import serviceAccount from '@/lib/secrets/firebase-admin-key.json'; // adjust path if needed
+const base64 = process.env.FIREBASE_ADMIN_KEY_BASE64!;
+const jsonString = Buffer.from(base64, 'base64').toString('utf-8');
+const serviceAccount = JSON.parse(jsonString);
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
